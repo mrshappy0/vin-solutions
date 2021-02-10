38838cbe499b049490b6a9f5e3f7c41c055f7c68
mrshappy0 <51244673+mrshappy0@users.noreply.github.com>
2021-02-09 19:33:37 -0700

    Finished code challenge

--- index.js
+++ index.js
@@ -2,15 +2,30 @@
 // Run npm run docs to run markdown magic. This auto-generates a code snippet within the readme doc.
 
 let stringManipulation = (string = "") => {
-	// Skip regex loop if empty. Default is set to empty
-	if (string === "") return "";
+  // Seperate at non-letter char. i-flag for case-insensitive.
+  // Filter removes empty (false-y) strings returned when seperating two non-letter characters.
+  let strArr = string.split(/([^a-z])/i).filter((i) => i);
 
-	// This regex pattern matches anything that is not a non-letter.
-	//It then replaces it with the length of the match and continues searching through the string
-	let regexPattern = /(\B[^!@#$%^&*()':;,./|\d\s]+\B)/g;
-	return string.replace(regexPattern, (match, index) => match.length);
+  for (let i = 0; i < strArr.length; i++) {
+    let word = strArr[i];
+    let wordLength = word.length;
+
+    // If word is too short, do nothing. e.g. 'it' => 'it' or "?" => "?"
+    if (wordLength > 2) {
+      let firstChar = word[0];
+	  // new Set("someString").size 
+      let midChars = new Set(word.slice(1, wordLength - 1)).size;
+      let endChar = word[wordLength - 1];
+
+	  // Replace mid-section with number of unqiue characters.
+      strArr[i] = firstChar + midChars + endChar;
+    }
+  }
+  return strArr.join("");
 };
 
+console.log(stringManipulation("This is the waYyyy"));
+
 module.exports = {
-	stringManipulation,
+  stringManipulation,
 };
--- test/someTest.test.js
+++ test/someTest.test.js
@@ -1,24 +1,52 @@
 const assert = require("chai").assert;
 const { stringManipulation } = require("../index");
+const esprima = require("esprima");
+const util = require("util");
 
-let firstString = "Hello VinSolutions!@";
-let secondString = "This! Is A Test!";
-let emptyString = "";
-console.log("Processing tests:", firstString, secondString);
+let f = () => {
+  let firstString =
+    "Hello Vin$olutions!@$% Here is my tesssSt string. Fi1led with typos to ensure all goes well!";
+  let secondString = "This! Is A Test!";
+  let thirdString = "This is the waYyyy!";
+  let mississippi = "Mississippi";
+  let emptyString = "";
+  let whiteSpace = "     ";
+  console.log("Processing tests:");
 
-describe("***********Initial tests***********", function () {
-	it(`1st test with string: ${firstString}`, function () {
-		assert.strictEqual(stringManipulation(firstString), "H3o V10s!@");
-	});
-	it(`2nd Test with string ${secondString}`, function () {
-		assert.strictEqual(stringManipulation(secondString), "T2s! Is A T2t!");
-	});
+  describe("***********Initial tests***********", () => {
+    it(`1st test with string: ${firstString}`, () => {
+      assert.strictEqual(
+        stringManipulation(firstString),
+        "H2o V1n$o6s!@$% H2e is my t3t s4g. Fi1l1d w2h t3s to e4e a1l g2s w2l!"
+      );
+    });
+    it(`2nd Test with string ${secondString}`, () => {
+      assert.strictEqual(stringManipulation(secondString), "T2s! Is A T2t!");
+    });
+    it(`3rd Test with string ${thirdString}`, () => {
+      assert.strictEqual(stringManipulation(thirdString), "T2s is t1e w3y!");
+    });
+    it(`4th Test with string ${mississippi}`, () => {
+      assert.strictEqual(stringManipulation(mississippi), "M3i");
+    });
+  });
+  describe("***********Edge Cases***********", () => {
+    it(`Empty String Test': ${emptyString}`, () => {
+      assert.strictEqual(stringManipulation(emptyString), "");
+    });
+    it(`White Space': ${whiteSpace}`, () => {
+      assert.strictEqual(stringManipulation(whiteSpace), "     ");
+    });
+  });
+};
+
+f();
+let s = (f + "").slice(6);
+
+let result = esprima.parseScript(s);
+let map = result.body[0].body.map((item) => {
+  if (item.type === "VariableDeclaration") {
+    return item.declarations[0].id.name;
+  }
 });
-describe("***********Edge Cases***********", function () {
-	it(`Empty String Test': ${emptyString}`, function () {
-		assert.strictEqual(stringManipulation(emptyString), "");
-	});
-	// it(`2nd Test with string ${secondString}`, function () {
-	// 	assert.strictEqual(stringManipulation(secondString), "T2s! Is A T2t!");
-	// });
-});
+console.log(map.slice(0, map.length - 3));
